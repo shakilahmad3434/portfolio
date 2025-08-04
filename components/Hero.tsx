d9a9bab13672bdbe1e6ff370afe4d7a94739e062
Shakil Ahmad <shakilahmad342100@gmail.com>
2025-08-04 16:59:14 +0530

    change photo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,10 +23,10 @@ const Hero = () => {
         </div>
 
         <div className='flex flex-col lg:flex-row mt-12 xl:mt-0'>
-            <div className='relative -top-12 xl:-top-40 lg:w-96 flex justify-center lg:block'>
-                <img src="/assets/img/all-images/header-img2.png" alt="" className='rounded w-full h-full object-cover' />
+            <div className='relative lg:-top-40 xl:-top-30 w-90 lg:w-74 xl:w-86 flex justify-center lg:block border-5 border-[#FB8500] rounded-full overflow-hidden'>
+                <img src="/assets/img/all-images/abc.png" alt="" className='rounded w-full h-full object-cover' />
             </div>
-            <div className='lg:w-1/2 pt-12 sm:ml-16 2xl:ml-30 pb-0'>
+            <div className='lg:w-1/2 pt-12 sm:ml-16 2xl:ml-38 pb-0'>
                 <p className='text-xl font-medium text-[#5D626F] opacity-80'>As a passionate Full Stack Developer, I specialize in building scalable web applications from scratch using modern technologies like React, Next.js, Node.js, Express, and MongoDB. With over 3.5+ years of experience, I craft user-centric solutions that merge design with functionality.</p>
                 <Button />
             </div>
